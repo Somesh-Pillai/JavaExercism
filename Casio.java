1518db4c30ba9ebe132b2d560d801de056981daf
Somesh <imsomeshp@gmail.com>
2022-01-11 22:03:41 +0530

    added calculator

--- Casio.java
+++ Casio.java
@@ -1,11 +1,16 @@
 class Calculator {
-    public int add(int num1, int num2) {
+    int num1, num2;
+    Calculator(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public int add() {
         return num1 + num2;
     }
 }
 public class Casio {
     public static void main(String []args){
-        Calculator obj = new Calculator();
-        System.out.println(obj.add(3, 7));
+        Calculator obj = new Calculator(4, 7);
+        System.out.println(obj.add());
     }
 }
