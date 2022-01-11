d17614437f0ca2c25df8b13d8951582e25114f35
Somesh <imsomeshp@gmail.com>
2022-01-11 22:21:23 +0530

    added calculator

--- Casio.java
+++ Casio.java
@@ -1,16 +1,29 @@
 class Calculator {
     int num1, num2;
+    static String brand; // we make static if that variable is shared among many objects and one update is applicable to all other objects.
     Calculator(int num1, int num2){
         this.num1 = num1;
         this.num2 = num2;
+
     }
     public int add() {
         return num1 + num2;
     }
+    public void show(){
+        System.out.println(num1 + " + " + num2 + " = " + add());
+        System.out.println("Brand: " + brand);
+    }
 }
 public class Casio {
     public static void main(String []args){
-        Calculator obj = new Calculator(4, 7);
-        System.out.println(obj.add());
+        Calculator obj1 = new Calculator(4, 7);
+        Calculator.brand = "Casio";  //class name can be used no object needed.
+        Calculator obj2 = new Calculator(12, 34);
+//        obj2.brand = "Casio";
+        obj1.show();
+        obj2.show();
+        obj2.brand = "Rado";
+        obj2.show();
+        obj1.show();
     }
 }
